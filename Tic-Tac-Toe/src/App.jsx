47d3ae0dabc1d215e239f5150ebc00607bb33a4b
Skull-Coder <skullthecoder@gmail.com>
2025-09-15 10:56:51 +0530

    Tic-Tac-Toe project

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,115 @@
+import { useState, useRef } from 'react'
+
+import './App.css'
+
+function App() {
+  const [player, setPlayer] = useState('X')
+  const [winner, setWinner] = useState("");
+
+  const movesRef = useRef(["", "", "", "", "", "", "", "", ""]);
+
+  const handleClick = (e) => {
+    const target = e.target;
+
+    if (winner) return;
+
+    console.log(target.tagName.toLowerCase());
+
+    if (target.className === "box") {
+      const isEmpty = target.textContent.trim() === "";
+
+      if (isEmpty) {
+        target.innerHTML = player;
+
+        movesRef.current[parseInt(target.id, 10)] = player;
+
+        if (isWon()) {
+          setWinner(player);  // store winner in state
+        } else {
+          setPlayer(player === 'X' ? 'O' : 'X');
+        }
+
+
+        console.log(movesRef);
+      }
+
+    }
+  };
+
+
+
+  function isWon() {
+    const moves = movesRef.current;
+
+    if (moves[0] !== "" && moves[0] === moves[1] && moves[1] === moves[2]) {
+      // alert(`Player ${moves[0]} has won the game!`);
+      return true;
+    }
+    else if (moves[3] !== "" && moves[3] === moves[4] && moves[4] === moves[5]) {
+      // alert(`Player ${moves[3]} has won the game!`);
+      return true;
+    }
+    else if (moves[6] !== "" && moves[6] === moves[7] && moves[7] === moves[8]) {
+      // alert(`Player ${moves[6]} has won the game!`);
+      return true;
+    }
+    else if (moves[0] !== "" && moves[0] === moves[3] && moves[3] === moves[6]) {
+      // alert(`Player ${moves[0]} has won the game!`);
+      return true;
+    }
+    else if (moves[1] !== "" && moves[1] === moves[4] && moves[4] === moves[7]) {
+      // alert(`Player ${moves[1]} has won the game!`);
+      return true;
+    }
+    else if (moves[2] !== "" && moves[2] === moves[5] && moves[5] === moves[8]) {
+      // alert(`Player ${moves[2]} has won the game!`);
+      return true;
+    }
+    else if (moves[0] !== "" && moves[0] === moves[4] && moves[4] === moves[8]) {
+      // alert(`Player ${moves[0]} has won the game!`);
+      return true;
+    }
+    else if (moves[2] !== "" && moves[2] === moves[4] && moves[4] === moves[6]) {
+      // alert(`Player ${moves[2]} has won the game!`);
+      return true;
+    }
+
+    // If no condition matches
+    return false;
+  }
+
+
+
+
+
+  return (
+    <>
+      <div className="container" >
+
+        <h1>Tic-Tac-Toe</h1>
+        <div className="app">
+          <p><b> {!winner ? `Next player: ${player}` : `${player} Won`}</b></p>
+
+          <div className="board" onClick={handleClick}>
+
+            <div className="box" id='0'></div>
+            <div className="box" id='1'></div>
+            <div className="box" id='2'></div>
+            <div className="box" id='3'></div>
+            <div className="box" id='4'></div>
+            <div className="box" id='5'></div>
+            <div className="box" id='6'></div>
+            <div className="box" id='7'></div>
+            <div className="box" id='8'></div>
+
+
+          </div>
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
